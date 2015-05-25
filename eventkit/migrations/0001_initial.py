2fa784c5276b418e3dba2702979be5fa0a5887a4
Tai Lee <real.human@mrmachine.net>
2015-05-25 12:50:25 +1000

    Don't require migrations that are only available in Django 1.8.

--- eventkit/migrations/0001_initial.py
+++ eventkit/migrations/0001_initial.py
@@ -9,7 +9,7 @@ import eventkit.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
