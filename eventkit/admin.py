f9d8caafc41832b1504d0054deb0cc5d27103a2d
Tai Lee <real.human@mrmachine.net>
2015-06-01 14:06:39 +1000

    Add dynamic preview for recurrence rule fields.

--- eventkit/admin.py
+++ eventkit/admin.py
@@ -5,15 +5,18 @@ Admin configuration for ``eventkit`` app.
 # Define `list_display`, `list_filter` and `search_fields` for each model.
 # These go a long way to making the admin more usable.
 
+from dateutil import rrule
 import datetime
 import json
+import six
 
 from django.contrib import admin
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import csrf_exempt
 from polymorphic.admin import \
     PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 from timezone import timezone
@@ -127,5 +130,41 @@ class RecurrenceRuleAdmin(admin.ModelAdmin):
         models.RecurrenceRuleField: {'widget': forms.RecurrenceRuleWidget},
     }
     model = models.RecurrenceRule
+
+    def get_urls(self):
+        """
+        Add a preview URL.
+        """
+        from django.conf.urls import patterns, url
+        urls = super(RecurrenceRuleAdmin, self).get_urls()
+        my_urls = patterns(
+            '',
+            url(
+                r'^preview/$',
+                self.admin_site.admin_view(self.preview),
+                name='eventkit_recurrencerule_preview'
+            ),
+        )
+        return my_urls + urls
+
+    @csrf_exempt
+    def preview(self, request):
+        """
+        Return a occurrences in JSON format up until the configured limit.
+        """
+        recurrence_rule = request.POST.get('recurrence_rule')
+        limit = int(request.POST.get('limit', 10))
+        try:
+            rruleset = rrule.rrulestr(
+                recurrence_rule, dtstart=timezone.now(), forceset=True)
+        except ValueError as e:
+            data = {
+                'error': six.text_type(e),
+            }
+        else:
+            data = {
+                'occurrences': rruleset[:limit]
+            }
+        return JsonResponse(data)
 
 admin.site.register(models.RecurrenceRule, RecurrenceRuleAdmin)
