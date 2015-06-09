d50d2ff8890838f1a0fc295e716cf334642af368
Tai Lee <real.human@mrmachine.net>
2015-06-09 20:57:00 +1000

    Use `FluentLayoutsMixin` from `icekit`.

--- eventkit/plugins/fluentevent/admin.py
+++ eventkit/plugins/fluentevent/admin.py
@@ -1,25 +1,8 @@
-from django.contrib import admin
-from django.template.loader import get_template
-from fluent_contents.admin import PlaceholderEditorAdmin
-from fluent_contents.analyzer import get_template_placeholder_data
+from icekit.admin import FluentLayoutsMixin
 
 from eventkit.admin import EventChildAdmin
 from eventkit.plugins.fluentevent.models import FluentEvent
 
 
-class FluentEventAdmin(EventChildAdmin, PlaceholderEditorAdmin):
-    change_form_template = 'icekit/admin/fluent_layouts_change_form.html'
+class FluentEventAdmin(FluentLayoutsMixin, EventChildAdmin):
     model = FluentEvent
-
-    class Media:
-        js = ('icekit/admin/js/fluent_layouts.js', )
-
-    def get_placeholder_data(self, request, obj):
-        """
-        Get placeholder data from layout.
-        """
-        if not obj:
-            template = 'eventkit_fluentevent/layouts/default.html'
-        else:
-            template = obj.layout.template_name
-        return get_template_placeholder_data(get_template(template))
