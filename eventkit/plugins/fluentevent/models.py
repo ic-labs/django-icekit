f940649119f79bc189b5782f99fb06f724bb74ae
Tai Lee <real.human@mrmachine.net>
2015-06-09 20:58:21 +1000

    Use `FluentFieldsMixin` from `icekit`.

--- eventkit/plugins/fluentevent/models.py
+++ eventkit/plugins/fluentevent/models.py
@@ -2,21 +2,10 @@
 Models for ``eventkit.plugins.fluentevent`` app.
 """
 
-from django.db import models
-from fluent_contents.models import ContentItemRelation, PlaceholderRelation
+from icekit.models import FluentFieldsMixin
 
 from eventkit.models import Event
 
 
-class AbstractFluentEvent(Event):
-    layout = models.ForeignKey('icekit.Layout', blank=True, null=True)
-
-    contentitem_set = ContentItemRelation()
-    placeholder_set = PlaceholderRelation()
-
-    class Meta:
-        abstract = True
-
-
-class FluentEvent(AbstractFluentEvent):
+class FluentEvent(Event, FluentFieldsMixin):
     pass
