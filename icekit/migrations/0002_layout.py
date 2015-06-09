65a904c45a91df8246f0fbfe2dd4a8641d5b435b
Tai Lee <real.human@mrmachine.net>
2015-06-09 20:48:44 +1000

    Update migration so `template_name` is the correct field type.

--- icekit/migrations/0002_layout.py
+++ icekit/migrations/0002_layout.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.utils.timezone
 import icekit.validators
+import fluent_pages.models.fields
 
 
 class Migration(migrations.Migration):
@@ -21,7 +22,7 @@ class Migration(migrations.Migration):
                 ('modified', models.DateTimeField(default=django.utils.timezone.now, editable=False, db_index=True)),
                 ('key', models.SlugField(help_text='A short name to identify the layout programmatically.', verbose_name='key')),
                 ('title', models.CharField(max_length=255, verbose_name='title')),
-                ('template_name', models.CharField(max_length=255, validators=[icekit.validators.template_name])),
+                ('template_name', fluent_pages.models.fields.TemplateFilePathField(verbose_name=b'template file', recursive=True, match=b'.*\\.html$', validators=[icekit.validators.template_name])),
             ],
             options={
                 'ordering': ('title',),
