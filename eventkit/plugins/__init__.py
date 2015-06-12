24797be3964a13337960172570b7f587dcc3883f
Tai Lee <real.human@mrmachine.net>
2015-06-12 11:36:12 +1000

    Code style. Wrapping.

--- eventkit/plugins/__init__.py
+++ eventkit/plugins/__init__.py
@@ -2,7 +2,8 @@ import six
 from icekit.plugins.base import BaseChildModelPlugin, PluginMount
 
 
-class EventChildModelPlugin(six.with_metaclass(PluginMount, BaseChildModelPlugin)):
+class EventChildModelPlugin(six.with_metaclass(
+        PluginMount, BaseChildModelPlugin)):
     """
     Mount point for ``Event`` child model plugins.
     """
