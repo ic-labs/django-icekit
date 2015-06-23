9606774a4f884b469b8c6fcf6693fbc715584d1f
Stuart Dines <stuart@interaction.net.au>
2015-06-23 16:29:56 +1000

    Update images admin with filters.

--- icekit/plugins/image/admin.py
+++ icekit/plugins/image/admin.py
@@ -5,6 +5,7 @@ from . import models
 
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal = ['categories', ]
+    list_filter = ['categories', 'is_active', ]
 
 
 admin.site.register(models.Image, ImageAdmin)
