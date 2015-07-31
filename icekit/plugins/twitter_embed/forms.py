42bfa6b69697c0c093a961df5708f477288a6efa
Stuart Dines <stuart@interaction.net.au>
2015-07-31 17:07:43 +1000

    Add model and firld information to form.

--- icekit/plugins/twitter_embed/forms.py
+++ icekit/plugins/twitter_embed/forms.py
@@ -1,9 +1,14 @@
 import re
 from django import forms
 from fluent_contents.forms import ContentItemForm
+from icekit.plugins.twitter_embed.models import TwitterEmbedItem
 
 
 class TwitterEmbedAdminForm(ContentItemForm):
+    class Meta:
+        model = TwitterEmbedItem
+        fields = '__all__'
+
     def clean_twitter_url(self):
         """
         Make sure the URL provided matches the twitter URL format.
@@ -16,4 +21,3 @@ class TwitterEmbedAdminForm(ContentItemForm):
                 raise forms.ValidationError('Please provide a valid twitter link.')
 
         return url
-
