ee53ec51d98802bf0bc55e70c39cc0918f2bb274
Matt Stevenson <mattoc@mattoc.com>
2015-08-12 11:21:55 +1000

    Update Blog model and content item matching

--- icekit/plugins/blog_post/content_plugins.py
+++ icekit/plugins/blog_post/content_plugins.py
@@ -3,7 +3,6 @@ Definition of the plugin.
 """
 from django.apps import apps
 from django.conf import settings
-from django.db.models.loading import get_model
 from django.utils.translation import ugettext_lazy as _
 from fluent_contents.extensions import ContentPlugin, plugin_pool
 
@@ -15,7 +14,7 @@ BLOG_MODEL = apps.get_model(*icekit_blog_model.rsplit('.', 1))
 if icekit_blog_model != default_blog_model:
     @plugin_pool.register
     class BlogPostPlugin(ContentPlugin):
-        model = get_model(getattr(settings, 'ICEKIT_BLOG_CONTENT_ITEM', 'blog_post.PostItem'))
+        model = apps.get_model(getattr(settings, 'ICEKIT_BLOG_CONTENT_ITEM', 'blog_post.BlogPostItem'))
         category = _('Blog')
         render_template = 'icekit/plugins/post/default.html'
         raw_id_fields = ['post', ]
