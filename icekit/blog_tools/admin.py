02931598cfaf2e28dbab636c800fb85c4a70db61
Matt Stevenson <mattoc@mattoc.com>
2015-08-18 14:21:14 +1000

    Related to 228608c7 we just unregister the default model

--- icekit/blog_tools/admin.py
+++ icekit/blog_tools/admin.py
@@ -2,11 +2,11 @@ from django.apps import apps
 from django.contrib import admin
 from django.conf import settings
 
+from .models import BlogPost
+
 
 class PostAdmin(admin.ModelAdmin):
     list_filter = ['is_active', ]
 
 
-icekit_blog_model = getattr(settings, 'ICEKIT_BLOG_MODEL', 'blog_tools.BlogPost')
-post_class = apps.get_model(*icekit_blog_model.rsplit('.', 1))
-admin.site.register(post_class, PostAdmin)
+admin.site.register(BlogPost, PostAdmin)
