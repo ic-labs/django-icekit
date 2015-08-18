1219f06103c556ced581bd54b423b4edafad9951
Matt Stevenson <mattoc@mattoc.com>
2015-08-18 11:06:42 +1000

    Avoid registering blog admin if we're overriding in a project

--- icekit/blog_tools/admin.py
+++ icekit/blog_tools/admin.py
@@ -6,7 +6,8 @@ from django.conf import settings
 class PostAdmin(admin.ModelAdmin):
     list_filter = ['is_active', ]
 
-
-icekit_blog_model = getattr(settings, 'ICEKIT_BLOG_MODEL', 'blog_tools.BlogPost')
-post_class = apps.get_model(*icekit_blog_model.rsplit('.', 1))
-admin.site.register(post_class, PostAdmin)
+default_blog_model = 'blog_tools.BlogPost'
+icekit_blog_model = getattr(settings, 'ICEKIT_BLOG_MODEL', default_blog_model)
+if icekit_blog_model == default_blog_model:
+    post_class = apps.get_model(*icekit_blog_model.rsplit('.', 1))
+    admin.site.register(post_class, PostAdmin)
