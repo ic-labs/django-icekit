7329e9392f0fa9dc6c16bd38ee2ccfff8617fe08
Stuart Dines <stuart@interaction.net.au>
2015-08-18 10:47:24 +1000

    Add  call for Python-3 support.

--- icekit/plugins/instagram_embed/abstract_models.py
+++ icekit/plugins/instagram_embed/abstract_models.py
@@ -64,7 +64,7 @@ class AbstractInstagramEmbedItem(ContentItem):
         """
         r = requests.get('http://api.instagram.com/publicapi/oembed/?url=%s' % self.url)
         if r.status_code is 200:
-            return json.loads(r.content)
+            return json.loads(r.content.decode())
         return r.content
 
     def get_thumbnail(self):
