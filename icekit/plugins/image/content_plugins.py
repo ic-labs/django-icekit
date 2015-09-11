9131a9f2349261900f056c1f920307b0fce176ad
Richard Antecki <richard@antecki.id.au>
2015-09-11 23:41:34 +1000

    Implement per-app/model template overrides for ImagePlugin.

--- icekit/plugins/image/content_plugins.py
+++ icekit/plugins/image/content_plugins.py
@@ -2,6 +2,7 @@
 Definition of the plugin.
 """
 from django.utils.translation import ugettext_lazy as _
+from django.template import loader
 from fluent_contents.extensions import ContentPlugin, plugin_pool
 
 from . import models
@@ -11,5 +12,15 @@ from . import models
 class ImagePlugin(ContentPlugin):
     model = models.ImageItem
     category = _('Image')
-    render_template = 'icekit/plugins/image/default.html'
     raw_id_fields = ['image', ]
+
+    def get_render_template(self, request, instance, **kwargs):
+        template = loader.select_template([
+            'icekit/plugins/image/%s_%s.html' % (
+                type(instance.parent)._meta.app_label,
+                type(instance.parent)._meta.model_name
+            ),
+            'icekit/plugins/image/%s.html' % type(
+                instance.parent)._meta.app_label,
+            'icekit/plugins/image/default.html'])
+        return template.name
