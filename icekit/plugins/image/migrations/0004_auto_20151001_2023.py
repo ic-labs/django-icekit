6f8384d89f0ddde1567f184f9fd229635180ff38
Greg Turner <greg@interaction.net.au>
2015-10-01 20:24:53 -0700

    Tweak of image to show title in title attribute. More flexible help text.

---
+++ icekit/plugins/image/migrations/0004_auto_20151001_2023.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('image', '0003_auto_20150623_0115'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='image',
+            name='alt_text',
+            field=models.CharField(help_text="A description of the image for users who don't see images", max_length=255),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='image',
+            name='title',
+            field=models.CharField(help_text='The title is shown in the "title" attribute', max_length=255, blank=True),
+            preserve_default=True,
+        ),
+    ]
