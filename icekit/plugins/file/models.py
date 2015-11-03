0d38b3359609cf787b4536f98dda52bc318f0379
Stuart Dines <stuart@interaction.net.au>
2015-11-03 12:52:11 +1100

    Add basic file store.

---
+++ icekit/plugins/file/models.py
@@ -0,0 +1,15 @@
+from . import abstract_models
+
+
+class File(abstract_models.AbstractFile):
+    """
+    A reusable file.
+    """
+    pass
+
+
+class FileItem(abstract_models.AbstractFileItem):
+    """
+    A file from the File model.
+    """
+    pass
