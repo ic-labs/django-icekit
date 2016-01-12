ecd0a671d68b9ed5c83b88a8aa5bfe5f47dfb3af
Greg Turner <greg@interaction.net.au>
2016-01-12 18:16:14 +1100

    Fix pagination error regarding integer division (float division is needed).

--- icekit/utils/pagination.py
+++ icekit/utils/pagination.py
@@ -1,6 +1,7 @@
 import math
 from django.utils import six
 from el_pagination.utils import get_page_numbers
+from pprint import pprint
 
 def describe_page_numbers(current_page, total_count, per_page, page_numbers_at_ends=3, pages_numbers_around_current=3):
     """
@@ -9,41 +10,68 @@ def describe_page_numbers(current_page, total_count, per_page, page_numbers_at_e
     to display only the immediate numbers around the start, end, and the current page.
 
     ```
-    >>> describe_page_numbers(1, 500, 10)
-    {
-        'numbers': [1, 2, 3, 4, None, 48, 49, 50],
-        'has_previous': False,
-        'has_next': True,
-        'previous_page': 0,
-        'current_page': 1,
-        'next_page': 2,
-        'total_count': 500,
-        'per_page': 10,
-    }
+    >>> pprint(describe_page_numbers(1, 500, 10))
+    {'current_page': 1,
+     'has_next': True,
+     'has_previous': False,
+     'next_page': 2,
+     'numbers': [1, 2, 3, 4, None, 48, 49, 50],
+     'per_page': 10,
+     'previous_page': 0,
+     'total_count': 500}
 
-    >>> describe_page_numbers(10, 500, 10)
-    {
-        'numbers': [1, 2, 3, None, 7, 8, 9, 10, 11, 12, 13, None, 48, 49, 50],
-        'has_previous': True,
-        'has_next': True,
-        'previous_page': 9,
-        'current_page': 10,
-        'next_page': 11,
-        'total_count': 500,
-        'per_page': 10,
-    }
+    >>> pprint(describe_page_numbers(10, 500, 10))
+    {'current_page': 10,
+     'has_next': True,
+     'has_previous': True,
+     'next_page': 11,
+     'numbers': [1, 2, 3, None, 7, 8, 9, 10, 11, 12, 13, None, 48, 49, 50],
+     'per_page': 10,
+     'previous_page': 9,
+     'total_count': 500}
 
-    >>> describe_page_numbers(50, 500, 10)
-    {
-        'numbers': [1, 2, 3, None, 47, 48, 49, 50],
-        'has_previous': True,
-        'has_next': False,
-        'previous_page': 49,
-        'current_page': 50,
-        'next_page': 51,
-        'total_count': 500,
-        'per_page': 10,
-    }
+    >>> pprint(describe_page_numbers(50, 500, 10))
+    {'current_page': 50,
+     'has_next': False,
+     'has_previous': True,
+     'next_page': 51,
+     'numbers': [1, 2, 3, None, 47, 48, 49, 50],
+     'per_page': 10,
+     'previous_page': 49,
+     'total_count': 500}
+
+    >>> pprint(describe_page_numbers(2, 40, 10))
+    {'current_page': 2,
+     'has_next': True,
+     'has_previous': True,
+     'next_page': 3,
+     'numbers': [1, 2, 3, 4],
+     'per_page': 10,
+     'previous_page': 1,
+     'total_count': 40}
+
+    # total_count / per_page == 0 - need float math
+    >>> pprint(describe_page_numbers(1, 1, 20))
+    {'current_page': 1,
+     'has_next': False,
+     'has_previous': False,
+     'next_page': 2,
+     'numbers': [1],
+     'per_page': 20,
+     'previous_page': 0,
+     'total_count': 1}
+
+    # empty results - total_count==0
+    >>> pprint(describe_page_numbers(1, 0, 20))
+    {'current_page': 1,
+     'has_next': False,
+     'has_previous': False,
+     'next_page': 2,
+     'numbers': [],
+     'per_page': 20,
+     'previous_page': 0,
+     'total_count': 0}
+
     ```
 
     :param current_page: the current page number (page numbers should start at 1)
@@ -57,7 +85,7 @@ def describe_page_numbers(current_page, total_count, per_page, page_numbers_at_e
     if total_count:
         page_numbers = get_page_numbers(
             current_page=current_page,
-            num_pages=int(math.ceil(total_count / per_page)),
+            num_pages=int(math.ceil(1.0 * total_count / per_page)),
             extremes=page_numbers_at_ends,
             arounds=pages_numbers_around_current,
         )
@@ -74,3 +102,7 @@ def describe_page_numbers(current_page, total_count, per_page, page_numbers_at_e
         'total_count': total_count,
         'per_page': per_page,
     }
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
