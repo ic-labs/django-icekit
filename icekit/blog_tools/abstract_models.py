295886ea42a69afbc5a66b96d714ffe4cf2d438b
Aram Dulyan <aram00@gmail.com>
2016-03-16 18:34:02 +1100

    Unpinned django-polymorphic, since fluent-contents now requires 0.8+
    The unpinned requirement django-fluent-contents now required django-
    polymorphic > 0.8, so we can't enforce < 0.8. Also we already have a fix
    for the backwards incompatibility.

--- icekit/blog_tools/abstract_models.py
+++ icekit/blog_tools/abstract_models.py
@@ -4,12 +4,14 @@ from django.utils.six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django_extensions.db.models import AutoSlugField, TimeStampedModel
 from fluent_contents.models import ContentItem
+
 try:
     # for django-polymorphic >= 0.8
     from polymorphic.models import PolymorphicModel
-except:
+except ImportError:
     # for django-polymorphic < 0.8
     from polymorphic import PolymorphicModel
+
 
 @python_2_unicode_compatible
 class AbstractBlogPost(PolymorphicModel, TimeStampedModel):
