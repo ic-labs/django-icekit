f38893bb9b1c2743a066677ef65bcd4bce19901d
Stuart Dines <me@stuartdines.com>
2016-03-17 22:18:52 +1100

    Create `dashboard` app.

---
+++ icekit/dashboard/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'icekit.dashboard.apps.DashboardConfig'
---
+++ icekit/dashboard/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class DashboardConfig(AppConfig):
+    name = 'icekit.dashboard'
+    label = 'icekit_dashboard'
+
+    verbose_name = 'Dashboard'
