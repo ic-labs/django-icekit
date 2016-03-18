d1371b06234289de864dc7bc43d11ce0e3f89537
Tai Lee <real.human@mrmachine.net>
2016-03-19 10:13:18 +1100

    Restore `new_test_image()` and `delete_test_image()` as deprecated functions.

--- icekit/utils/testing.py
+++ icekit/utils/testing.py
@@ -2,10 +2,13 @@ import contextlib
 import glob
 import os
 import uuid
+import warnings
 
 from django.conf import settings
+from django.core.files.base import ContentFile
 from nose.tools import nottest
 from PIL import Image, ImageDraw
+from six import BytesIO
 
 
 @nottest
@@ -50,3 +53,51 @@ def get_test_image(storage):
 
     # Delete image.
     storage.delete(storage_name)
+
+
+# DEPRECATED ##################################################################
+
+
+@nottest
+def new_test_image():
+    """
+    Creates an automatically generated test image.
+    In your testing `tearDown` method make sure to delete the test
+    image with the helper function `delete_test_image`.
+    The recommended way of using this helper function is as follows:
+        object_1.image_property.save(*new_test_image())
+    :return: Image name and image content file.
+    """
+    warnings.warn(DeprecationWarning(
+        "new_test_image() is deprecated in favour of the get_sample_image() "
+        "context manager."), stacklevel=2)
+    image_name = 'test-{}.png'.format(uuid.uuid4())
+    image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+    ImageDraw.Draw(image)
+    byte_io = BytesIO()
+    image.save(byte_io, 'png')
+    byte_io.seek(0)
+    return image_name, ContentFile(byte_io.read(), image_name)
+
+
+@nottest
+def delete_test_image(image_field):
+    """
+    Deletes test image generated as well as thumbnails if created.
+    The recommended way of using this helper function is as follows:
+        delete_test_image(object_1.image_property)
+    :param image_field: The image field on an object.
+    :return: None.
+    """
+    warnings.warn(DeprecationWarning(
+        "delete_test_image() is deprecated in favour of the "
+        "get_sample_image() context manager."), stacklevel=2)
+    # ensure all thumbs are deleted
+    for filename in glob.glob(
+        os.path.join(
+            settings.BASE_DIR, 'public', 'media', 'thumbs', image_field.name.split('/')[-1]
+        ) + '*'
+    ):
+        os.unlink(filename)
+    # delete the saved file
+    image_field.delete()
--- icekit/utils/tests.py
+++ icekit/utils/tests.py
@@ -35,3 +35,31 @@ class TestingUtils(WebTest):
         # Check that the image and thumbnails are deleted.
         self.assertFalse(os.path.exists(image_test.image.path))
         self.assertFalse(os.path.exists(thumbnail))
+
+    def test_test_image_create_and_delete(self):
+        # Create the image model object.
+        image_test = G(ImageTest)
+
+        # Save the image field with the test image function to create an actual image file.
+        image_test.image.save(*testing.new_test_image())
+
+        # Check that the file was created as intended.
+        image_path = image_test.image.path
+        self.assertTrue(os.path.exists(image_path))
+
+        # Generate a fake thumbnail style file to test cleanup.
+        dest_dir = os.path.join(settings.BASE_DIR, 'public', 'media', 'thumbs')
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        shutil.copy(image_path, dest_dir)
+        dst_file = '%s/%s' % (dest_dir, image_test.image.name.split('/')[1])
+        dst_thumb_file = '%s.1800x1800_q85.png' % dst_file
+        os.rename(dst_file, dst_thumb_file)
+        self.assertTrue(os.path.exists(dst_thumb_file))
+
+        # Ensure that the delete function removes the image
+        testing.delete_test_image(image_test.image)
+        self.assertFalse(os.path.exists(image_path))
+
+        # Ensure that the delete function remove the thumbnail style images.
+        self.assertFalse(os.path.exists(dst_thumb_file))
