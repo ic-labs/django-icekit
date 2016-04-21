b89d0ffdc6bf5522d2aca02dab6ec99ee6dcc8b7
James Murty <james@murty.co>
2016-04-21 10:48:09 +1000

    Improve command for one-time DB migration to ICEKit publishing, re #5 #23

--- icekit/management/commands/publishing_migrate_from_publisher.py
+++ icekit/management/commands/publishing_migrate_from_publisher.py
@@ -9,8 +9,16 @@ class Command(NoArgsCommand):
     def handle_noargs(self, *args, **options):
         UrlNode = apps.get_model('fluent_pages.UrlNode')
         for node in UrlNode.objects.all():
+            # Copy legacy 'publisher' fields to 'publishing' equivalents
             node.publishing_linked = node.publisher_linked
             node.publishing_is_draft = node.publisher_is_draft
             node.publishing_modified_at = node.publisher_modified_at
             node.publishing_published_at = node.publisher_published_at
+
+            # Delete/clear legacy 'publisher_linked' field to avoid duplicate
+            # key errors for new publishing system, which no longer does all
+            # the fancy footwork necessary to handle the legacy `publisher`
+            # fields.
+            node.publisher_linked = None
+
             node.save()
