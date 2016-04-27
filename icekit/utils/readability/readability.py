dc04141126003dc0ce2786bf3a75055633dfcdfa
Greg Turner <greg@interaction.net.au>
2016-04-27 12:23:38 +1000

    Remove unused code that isn't Py3 compatible

--- icekit/utils/readability/readability.py
+++ icekit/utils/readability/readability.py
@@ -90,19 +90,19 @@ class Readability:
             score = longwords / self.analyzedVars['sentence_cnt']
         return score
 
-
-if __name__ == "__main__":
-    text = """We are close to wrapping up our 10 week Rails Course. This week we will cover a handful of topics commonly encountered in Rails projects. We then wrap up with part 2 of our Reddit on Rails exercise!  By now you should be hard at work on your personal projects. The students in the course just presented in front of the class with some live demos and a brief intro to to the problems their app were solving. Maybe set aside some time this week to show someone your progress, block off 5 minutes and describe what goal you are working towards, the current state of the project (is it almost done, just getting started, needs UI, etc.), and then show them a quick demo of the app. Explain what type of feedback you are looking for (conceptual, design, usability, etc.) and see what they have to say.  As we are wrapping up the course you need to be focused on learning as much as you can, but also making sure you have the tools to succeed after the class is over."""
-
-    rd = Readability(text)
-    print 'Test text:'
-    print '"%s"\n' % text
-    print 'ARI: ', rd.ARI()
-    # print 'FleschReadingEase: ', rd.FleschReadingEase()
-    # print 'FleschKincaidGradeLevel: ', rd.FleschKincaidGradeLevel()
-    print 'GunningFogIndex: ', rd.GunningFogIndex()
-    print 'SMOGIndex: ', rd.SMOGIndex()
-    # print 'ColemanLiauIndex: ', rd.ColemanLiauIndex()
-    # print 'LIX: ', rd.LIX()
-    # print 'RIX: ', rd.RIX()
-
+# commenting for quick py3 compatibility
+# if __name__ == "__main__":
+#     text = """We are close to wrapping up our 10 week Rails Course. This week we will cover a handful of topics commonly encountered in Rails projects. We then wrap up with part 2 of our Reddit on Rails exercise!  By now you should be hard at work on your personal projects. The students in the course just presented in front of the class with some live demos and a brief intro to to the problems their app were solving. Maybe set aside some time this week to show someone your progress, block off 5 minutes and describe what goal you are working towards, the current state of the project (is it almost done, just getting started, needs UI, etc.), and then show them a quick demo of the app. Explain what type of feedback you are looking for (conceptual, design, usability, etc.) and see what they have to say.  As we are wrapping up the course you need to be focused on learning as much as you can, but also making sure you have the tools to succeed after the class is over."""
+#
+#     rd = Readability(text)
+#     print 'Test text:'
+#     print '"%s"\n' % text
+#     print 'ARI: ', rd.ARI()
+#     # print 'FleschReadingEase: ', rd.FleschReadingEase()
+#     # print 'FleschKincaidGradeLevel: ', rd.FleschKincaidGradeLevel()
+#     print 'GunningFogIndex: ', rd.GunningFogIndex()
+#     print 'SMOGIndex: ', rd.SMOGIndex()
+#     # print 'ColemanLiauIndex: ', rd.ColemanLiauIndex()
+#     # print 'LIX: ', rd.LIX()
+#     # print 'RIX: ', rd.RIX()
+#
