a0741eaad2f9172abf350a6d4b2c8b7e8d08e4db
James Murty <james@murty.co>
2016-05-18 21:55:16 +1000

    Make publisher migration command work with non-legacy models, re #5 #23
    The 'publisher' => 'publishing' DB migration command now works when the
    legacy 'publisher' fields have been removed from the code (and models)
    of the legacy system, provided they still remain in the database.

--- icekit/management/commands/publishing_migrate_from_publisher.py
+++ icekit/management/commands/publishing_migrate_from_publisher.py
@@ -1,5 +1,10 @@
 from django.apps import apps
 from django.core.management.base import NoArgsCommand
+from django.db import connection
+
+from fluent_pages.models.db import UrlNode
+
+from icekit.publishing.utils import get_publishable_models
 
 
 class Command(NoArgsCommand):
@@ -7,18 +12,90 @@ class Command(NoArgsCommand):
             " DB fields to ICEKit's 'publishing' equivalents")
 
     def handle_noargs(self, *args, **options):
-        UrlNode = apps.get_model('fluent_pages.UrlNode')
-        for node in UrlNode.objects.all():
-            # Copy legacy 'publisher' fields to 'publishing' equivalents
-            node.publishing_linked = node.publisher_linked
-            node.publishing_is_draft = node.publisher_is_draft
-            node.publishing_modified_at = node.publisher_modified_at
-            node.publishing_published_at = node.publisher_published_at
+        publishable_models = get_publishable_models()
+        count = len(publishable_models)
+        for offset, model in enumerate(publishable_models, 1):
+            print("Migrating publisher => publishing for %s (%d/%d)"
+                  % (model, offset, count))
 
-            # Set `UrlNode.status` based on published status
-            if getattr(node, 'publisher_is_draft', True):
-                node.status = UrlNode.DRAFT
-            else:
-                node.status = UrlNode.PUBLISHED
+            # Try and do a simple and fast in-DB bulk update query first, for
+            # cases where the legacy and new fields are in the same DB table
+            try:
+                cursor = connection.cursor()
+                cursor.execute(
+                    """
+                    UPDATE %s
+                       SET publishing_linked_id = publisher_linked_id,
+                           publishing_is_draft = publisher_is_draft,
+                           publishing_modified_at = publisher_modified_at,
+                           publishing_published_at = publisher_published_at
+                    """ % (model._meta.db_table)
+                )
+                continue
+            except Exception, ex:
+                pass
 
-            node.save()
+            # Second, try an in-DB bulk update for models that are subclasses
+            # of `UrlNode` and for which the legacy publisher fields are in
+            # the `UrlNode` DB table.
+            if issubclass(model, UrlNode):
+                try:
+                    cursor = connection.cursor()
+                    cursor.execute(
+                        """
+                        SELECT t.{pk_col},
+                            publisher_linked_id,
+                            publisher_is_draft,
+                            publisher_modified_at,
+                            publisher_published_at
+                        FROM {table} t
+                        JOIN {urlnode_table} ON t.{pk_col} = {urlnode_table}.id
+                        """.format(pk_col=model._meta.pk.column,
+                                    table=model._meta.db_table,
+                                    urlnode_table=UrlNode._meta.db_table)
+                    )
+                    continue
+                except Exception, ex:
+                    pass
+
+            # Finally, try doing the data migration with Django model
+            # instances if we get this far. This is very slow, but works
+            # for models where the publishing fields are defined on
+            # a superclass not the exact model.
+            publisher_fields_by_pk = {}
+
+            # Lookup legacy 'publisher' fields with direct DB query
+            try:
+                cursor = connection.cursor()
+                cursor.execute(
+                    """
+                    SELECT %s,
+                        publisher_linked_id,
+                        publisher_is_draft,
+                        publisher_modified_at,
+                        publisher_published_at
+                    FROM %s
+                    """ % (model._meta.pk.column, model._meta.db_table)
+                )
+            except Exception, ex:
+                print("WARNING: Could not migrate data for %s" % model)
+                continue
+
+            for row in cursor.fetchall():
+                publisher_fields_by_pk[row[0]] = row[1:]
+
+            for item in model.objects.all():
+                publisher_fields = publisher_fields_by_pk[item.pk]
+                # Copy legacy 'publisher' fields to 'publishing' equivalents
+                item.publishing_linked_id = publisher_fields[0]
+                item.publishing_is_draft = publisher_fields[1]
+                item.publishing_modified_at = publisher_fields[2]
+                item.publishing_published_at = publisher_fields[3]
+
+                # Set `UrlNode.status` based on published status
+                if getattr(item, 'publisher_is_draft', True):
+                    item.status = UrlNode.DRAFT
+                else:
+                    item.status = UrlNode.PUBLISHED
+
+                item.save()
