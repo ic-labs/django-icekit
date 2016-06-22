3074b287ec5f660ceef923ef4d16f8eec1cd8504
Greg Turner <greg@interaction.net.au>
2016-06-22 21:24:30 +1000

    Also removing blog from tests

--- icekit/tests/urls.py
+++ icekit/tests/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns(
     # url(r'^forms/', include(forms_builder.forms.urls)),
     url(r'^404/$', 'icekit.response_pages.views.page_not_found', name='404'),
     url(r'^500/$', 'icekit.response_pages.views.server_error', name='500'),
-    url(r'blog/', include('icekit.blog_tools.urls')),
     url(r'', include('icekit.urls')),
     url(r'', include('fluent_pages.urls')),
 )
