c8f705458c94c7415d188c160d87fd660f26e0e5
Greg Turner <greg@interaction.net.au>
2016-06-22 21:05:09 +1000

    Removing blog_tools and blog_post re #20

--- icekit/blog_tools/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import BlogDetailView, BlogListView
-
-
-urlpatterns = patterns('',
-    url(r'^(?P<pk>[0-9]+)/$', BlogDetailView.as_view(), name='detail'),
-    url(r'^$', BlogListView.as_view(), name='list'),
-)
