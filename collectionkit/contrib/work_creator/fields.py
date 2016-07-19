96a420d49bd536337a67c92a74bf26540bff0acc
Greg Turner <greg@interaction.net.au>
2016-07-19 21:53:33 +1000

    Catch any error trying to update width/height fields.

--- collectionkit/contrib/work_creator/fields.py
+++ collectionkit/contrib/work_creator/fields.py
@@ -1,11 +1,10 @@
 from django.db.models import ImageField
 
-
 class QuietImageField(ImageField):
     """An imagefield that doesn't lose the plot when trying to find the
     dimensions of an image that doesn't exist"""
     def update_dimension_fields(self, *args, **kwargs):
         try:
             super(QuietImageField, self).update_dimension_fields(*args, **kwargs)
-        except IOError:
+        except:
             pass
