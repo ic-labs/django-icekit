5a3f072861e9f21ddcd33cc3bfe17d4a914e040f
Tai Lee <real.human@mrmachine.net>
2016-08-11 01:07:59 +1000

    Import project settings from right place.

--- icekit/project/settings/__init__.py
+++ icekit/project/settings/__init__.py
@@ -1,7 +1,9 @@
+from __future__ import absolute_import
+
 import os
 
 try:
-    from project.settings import *
+    from settings import *
 except ImportError:
     os.environ.setdefault('BASE_SETTINGS_MODULE', 'develop')
     from .calculated import *
