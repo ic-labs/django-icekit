9e67a7453b2d604cb1e6f1652df2bc02ea81d9c3
Tai Lee <real.human@mrmachine.net>
2016-08-12 16:10:06 +1000

    Add startproject script.

---
+++ bin/startproject.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+DEST_DIR="${1:-$PWD}"
+
+cat <<EOF
+
+This script will create a new ICEkit project in directory '${DEST_DIR}'.
+
+You will be able to run it immediately with Docker, or you can manually install
+its dependencies and run it directly.
+
+EOF
+
+read -p 'Press CTRL-C to abort or any other key to continue...'
+echo
+
+if [[ -z $(which curl) && -z $(which wget) ]]; then
+    cat <<EOF
+
+Neither 'curl' nor 'wget' is available. Please install one or the other and try
+again.
+
+EOF
+    exit 1
+fi
+
+mkdir -p "$DEST_DIR"
+cd "$DEST_DIR"
+
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/bower.json
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/docker-compose.override.yml
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/docker-compose.yml
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/docker-stack.sample.yml
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/Dockerfile
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/go.sh
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/package.json
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/README.md
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/requirements.txt
+wget -nv https://raw.githubusercontent.com/ic-labs/django-icekit/master/icekit/project_template/settings.py
+
+if [[ -n $(which git) ]]; then
+    echo
+    read -p 'Would you like to initialize a Git repository for your new project and create an initial commit? (Y/n) ' -n 1 -r
+    echo
+    if [[ "${REPLY:-y}" =~ ^[Yy]$ ]]; then
+        git init
+        git add -A
+        git commit -m 'Initial commit.'
+    fi
+fi
+
+cat <<EOF
+
+All done!
+
+If you have Docker installed, you can run your new ICEkit project immediately:
+
+    $ cd ${DEST_DIR}
+    $ docker-compose up
+
+Open the site in a browser:
+
+    http://lvh.me
+
+Or to run directly, go to https://github.com/ic-labs/django-icekit/#run-directly
+
+EOF
