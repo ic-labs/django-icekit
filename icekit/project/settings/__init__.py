52a326c4d593b390d6e7788907b3697c2e4fd81d
Tai Lee <real.human@mrmachine.net>
2016-08-13 13:27:10 +1000

    Rename project settings module to avoid conflict with django-dynamic-fixture.

--- icekit/project/settings/__init__.py
+++ icekit/project/settings/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import os
 
 try:
-    from settings import *
+    from icekit_settings import *
 except ImportError:
     os.environ.setdefault('BASE_SETTINGS_MODULE', 'develop')
     from .calculated import *
