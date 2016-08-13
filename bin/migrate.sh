7698d812edc0983f50f18cfd56032a3a1a730b53
Tai Lee <real.human@mrmachine.net>
2016-08-14 00:20:06 +1000

    Update `migrate.sh` script now that `manage.py` is not installed as a script, but must be executed from the ICEkit source or project root.

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -11,16 +11,15 @@ set -e
 DIR="${1:-$PWD}"
 
 mkdir -p "$DIR"
-cd "$DIR"
 
-touch migrate.txt.md5
-manage.py migrate --list > migrate.txt
+touch "$DIR/migrate.txt.md5"
+python manage.py migrate --list > "$DIR/migrate.txt"
 
-if ! md5sum -c --status migrate.txt.md5 > /dev/null 2>&1; then
+if ! md5sum -c --status "$DIR/migrate.txt.md5" > /dev/null 2>&1; then
     echo 'Migrations are out of date.'
-    manage.py migrate --noinput
-    manage.py migrate --list > migrate.txt
-    md5sum migrate.txt > migrate.txt.md5
+    python manage.py migrate --noinput
+    python manage.py migrate --list > "$DIR/migrate.txt"
+    md5sum migrate.txt > "$DIR/migrate.txt.md5"
 else
     echo 'Migrations are already up to date.'
 fi
