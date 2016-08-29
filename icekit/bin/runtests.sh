13d01370765bb5ca0200aaabc906ac20881e6387
Tai Lee <real.human@mrmachine.net>
2016-08-29 15:29:29 +1000

    Use absolute path to `initial_data.sql` (previously `test_icekit.sql`).

--- icekit/bin/runtests.sh
+++ icekit/bin/runtests.sh
@@ -12,6 +12,6 @@ export BASE_SETTINGS_MODULE=test
 export PGDATABASE=test_icekit
 export REUSE_DB=1
 export FORCE_SETUP_POSTGRES_DATABASE=1
-export SRC_PGDATABASE=test_icekit.sql
+export SRC_PGDATABASE="$ICEKIT_DIR/initial_data.sql"
 
 exec entrypoint.sh python "$ICEKIT_DIR/bin/manage.py" test --noinput --verbosity=2 "$@"
