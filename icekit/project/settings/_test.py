70a40f50e9988fadfbc42f236881c1e3e78f40f1
Tai Lee <real.human@mrmachine.net>
2016-08-30 12:40:13 +1000

    Extend base settings for test settings. Don't use live cache backend for tests.

--- icekit/project/settings/_test.py
+++ icekit/project/settings/_test.py
@@ -1,6 +1,11 @@
-from ._develop import *
+from ._base import *
 
 # DJANGO ######################################################################
+
+ALLOWED_HOSTS = ('*', )
+
+CSRF_COOKIE_SECURE = False  # Don't require HTTPS for CSRF cookie
+SESSION_COOKIE_SECURE = False  # Don't require HTTPS for session cookie
 
 DATABASES['default'].update({
     'TEST': {
