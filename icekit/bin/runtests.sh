c88bc110d41daba4a9fc72941393d426b61f5de7
Tai Lee <real.human@mrmachine.net>
2016-08-30 23:21:14 +1000

    Run `collectstatic` and `compress` in `runtests.sh`. It seems to be required.

--- icekit/bin/runtests.sh
+++ icekit/bin/runtests.sh
@@ -16,6 +16,8 @@ export SRC_PGDATABASE="$ICEKIT_DIR/initial_data.sql"
 
 unset WAITLOCK_ENABLED
 
+manage.py collectstatic --noinput --verbosity=0
+
 coverage run "$ICEKIT_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-icekit}"
 coverage report
 
