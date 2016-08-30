410f591e5f5ccceba78c2cba2823c1ab63c34830
Tai Lee <real.human@mrmachine.net>
2016-08-30 12:42:41 +1000

    Don't say we're waiting for a lock when we're not.

--- icekit/bin/waitlock.sh
+++ icekit/bin/waitlock.sh
@@ -3,16 +3,16 @@
 # Execute cronlock in a loop to queue but avoid overlaps, instead of exiting
 # when a lock is already acquired.
 
+# Don't use Redis locks to serialize access to shared resources, e.g. database.
+if [[ -z "$WAITLOCK_ENABLE" ]]; then
+    exec "$@"
+fi
+
 cat <<EOF
 # `whoami`@`hostname`:$PWD$ waitlock.sh $@
 EOF
 
 set -e
-
-# Don't use Redis locks to serialize access to shared resources, e.g. database.
-if [[ -z "$WAITLOCK_ENABLE" ]]; then
-    exec "$@"
-fi
 
 export CRONLOCK_GRACE="${CRONLOCK_GRACE:-0}"
 export CRONLOCK_HOST="${CRONLOCK_HOST:-localhost}"
