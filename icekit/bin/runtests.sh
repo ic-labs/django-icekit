01905f91a0d1288edda1d84f6604957774f24a41
Tai Lee <real.human@mrmachine.net>
2016-08-30 23:17:59 +1000

    Generate coverage data/report in `runtests.sh`, and submit (if run on Travis CI) to coveralls.

--- icekit/bin/runtests.sh
+++ icekit/bin/runtests.sh
@@ -16,4 +16,9 @@ export SRC_PGDATABASE="$ICEKIT_DIR/initial_data.sql"
 
 unset WAITLOCK_ENABLED
 
-python "$ICEKIT_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-icekit}"
+coverage run "$ICEKIT_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-icekit}"
+coverage report
+
+if [[ -n "$TRAVIS" ]]; then
+    coveralls
+fi
