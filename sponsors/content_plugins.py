2db9f045c2d7a62ceec0ce7da7eed028b8f713da
Greg Turner <greg@interaction.net.au>
2016-09-05 11:45:11 +1000

    Templates for content plugins.

--- sponsors/content_plugins.py
+++ sponsors/content_plugins.py
@@ -6,19 +6,19 @@ from . import models
 @plugin_pool.register
 class BeginSponsorBlockPlugin(ContentPlugin):
     model = models.BeginSponsorBlockItem
-    render_template = 'sfmoma/plugins/sponsor_block/begin.html'
+    render_template = 'plugins/sponsor_block/begin.html'
     category = _('Sponsorship')
 
 @plugin_pool.register
 class EndSponsorBlockPlugin(ContentPlugin):
     model = models.EndSponsorBlockItem
-    render_template = 'sfmoma/plugins/sponsor_block/end.html'
+    render_template = 'plugins/sponsor_block/end.html'
     category = _('Sponsorship')
 
 
 @plugin_pool.register
 class SponsorPromoPlugin(ContentPlugin):
     model = models.SponsorPromoItem
-    render_template = 'sfmoma/plugins/sponsor_block/sponsor.html'
+    render_template = 'plugins/sponsor_block/sponsor.html'
     category = _('Sponsorship')
     raw_id_fields = ['sponsor', ]
