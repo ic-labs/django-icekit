93d5144703464393806dda7c81e7704e7ecaf528
Tai Lee <real.human@mrmachine.net>
2016-09-27 13:57:42 +1000

    Don't reuse DB and collected static and compressed files by default.
    The default is safe, consistent (and slower). If you want to run tests
    faster, set `QUICK=1`:
    
        # QUICK=1 runtests.sh

--- icekit/bin/runtests.sh
+++ icekit/bin/runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Run tests.
+# Run tests. Set 'QUICK=1' reuse the test database and collected static and
+# compressed files.
 
 cat <<EOF
 # `whoami`@`hostname`:$PWD$ runtests.sh $@
@@ -12,11 +13,15 @@ export BASE_SETTINGS_MODULE=test
 export REUSE_DB=1
 export SRC_PGDATABASE="$ICEKIT_PROJECT_DIR/test_initial_data.sql"
 
+[[ -z "$QUICK" ]] && export SETUP_POSTGRES_FORCE=1
+
 PGDATABASE="test_$PGDATABASE" setup-postgres.sh
 manage.py migrate --noinput
 
-[[ ! -d "$ICEKIT_PROJECT_DIR/static_root" ]] && manage.py collectstatic --noinput --verbosity=0
-[[ ! -f "$ICEKIT_PROJECT_DIR/static_root/CACHE/manifest.json" ]] && manage.py compress --verbosity=0
+if [[ -n "$QUICK" ]]; then
+    [[ ! -d "$ICEKIT_PROJECT_DIR/static_root" ]] && manage.py collectstatic --noinput --verbosity=0
+    [[ ! -f "$ICEKIT_PROJECT_DIR/static_root/CACHE/manifest.json" ]] && manage.py compress --verbosity=0
+fi
 
 coverage run "$ICEKIT_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-.}"
 coverage report
