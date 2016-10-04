8d124ece058bf3450e71ef96dd6116503f21cec6
James Murty <james@murty.co>
2016-10-04 16:30:05 +1100

    Workaround issue with django-polymorphic and Fluent pages admin, re #31
    Add an improved implementation of django-polymorphic's
    `PolymorphicChildModelAdmin._get_parent_admin` method for Fluent Pages
    that does not require the absolute base polymorphic parent `UrlNode` to
    be have a polymorphic parent admin registered, but will instead search
    for polymorphic parent admins registered for intermediate models in the
    class hierachy – specifically `Page` – which is where we really want to
    anchor the parent admin.
    
    Use the `PolymorphicChildModelAdminGetParentAdminFix` mixin to fix the
    `ParentAdminNotRegistered` error produced by django-polymorphic versions
    1.0+ when you *Save* (not *Save and continue*) a Page child model.

--- icekit/tests/tests.py
+++ icekit/tests/tests.py
@@ -491,6 +491,16 @@ class Views(WebTest):
                 user=self.super_user_1)
             self.assertEqual(response.status_code, 200)
 
+    # Test workaround applied for django-polymorphic 1.0+ per #31
+    def test_can_save_polymorphic_page_in_admin(self):
+        response = self.app.get(
+            reverse('admin:fluent_pages_page_change',
+                    args=(self.layoutpage_1.pk,)),
+            user=self.super_user_1)
+        # Hit 'Save' in form -- does nothing really, but will fail with
+        # `ParentAdminNotRegistered` if polymorphic issue #31 is present.
+        response.forms[0].submit()
+
     def test_response_pages(self):
         response = self.app.get(reverse('404'), expect_errors=404)
         self.assertEqual(response.status_code, 404)
