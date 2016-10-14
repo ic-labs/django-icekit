7cfebe57fb0bc57829f4ec445e3a55d98ba26107
Tai Lee <real.human@mrmachine.net>
2016-10-14 11:20:51 +1100

    Add `icekit` and `glamkit` settings modules, to be imported by `project_settings` (formerly `icekit_settings`).

---
+++ project_template/project_settings.py
@@ -0,0 +1,9 @@
+# Do not commit secrets to VCS.
+
+# Local environment variables will be loaded from `.env.local`.
+# Additional environment variables will be loaded from `.env.$DOTENV`.
+# Local settings will be imported from `icekit_settings_local.py`
+
+from icekit.project.settings.icekit import *  # icekit, glamkit
+
+# Override the default ICEkit settings to form project settings.
