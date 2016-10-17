af4ad27ddf4d5da23590f6b2e297b9d834fa292e
James Murty <james@murty.co>
2016-10-17 16:35:50 +1100

    Add ICEKit Events to list of GLAMKit installed apps
    Add ICEKit events, SimpleEvent event type, and listing page for date
    apps to those installed by default for GLAMKit.

--- icekit/project/settings/glamkit.py
+++ icekit/project/settings/glamkit.py
@@ -2,4 +2,10 @@ from .icekit import *
 
 # DJANGO ######################################################################
 
-INSTALLED_APPS += ('sponsors', )
+INSTALLED_APPS += (
+    'sponsors',
+
+    'icekit_events',
+    'icekit_events.event_types.simple',
+    'icekit_events.page_types.eventlistingfordate',
+)
