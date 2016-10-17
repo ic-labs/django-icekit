8e7a3d78f433c187acb8806c8f27d2b83853ca89
James Murty <james@murty.co>
2016-10-17 14:30:58 +1100

    Fix and merge SimpleEvent DB migrations
    - adjust DB migrations to account for `Event` => `EventBase` rename
    - combine two migrations into one to avoid trivial second migration.

--- icekit_events/event_types/simple/migrations/0001_initial.py
+++ icekit_events/event_types/simple/migrations/0001_initial.py
@@ -7,19 +7,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icekit_events', '0018_auto_20160929_2106'),
+        ('icekit_events', '0017_rename_event_to_eventbase'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='SimpleEvent',
             fields=[
-                ('event_ptr', models.OneToOneField(serialize=False, primary_key=True, auto_created=True, parent_link=True, to='icekit_events.Event')),
+                ('eventbase_ptr', models.OneToOneField(serialize=False, primary_key=True, auto_created=True, parent_link=True, to='icekit_events.EventBase')),
             ],
             options={
                 'abstract': False,
                 'ordering': ('title', 'pk'),
+                'verbose_name': 'Simple event',
             },
-            bases=('icekit_events.event',),
+            bases=('icekit_events.eventbase',),
         ),
     ]
