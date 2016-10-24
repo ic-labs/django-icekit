6639b33ddee9c6d880e1b8c777f6795efcf76f3f
Greg Turner <greg@interaction.net.au>
2016-10-24 18:19:30 +1100

    Add utilities for quickly getting the admin url/link for a model instance.

---
+++ icekit/utils/admin/urls.py
@@ -0,0 +1,24 @@
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
+from django.template import Template, Context
+from django.utils.safestring import mark_safe
+
+
+def admin_url(inst):
+    """
+    :param inst: An Model Instance
+    :return: the admin URL for the instance. Permissions aren't checked.
+    """
+    if inst:
+        t = Template("""{% load admin_urls %}{% url opts|admin_urlname:'change' inst.pk %}""")
+        return t.render(Context({ 'inst': inst, 'opts': inst._meta}))
+    return ""
+
+def admin_link(inst):
+    """
+    :param inst: An Model Instance
+    :return: a complete admin link for the instance. Permissions aren't checked.
+    """
+    if inst:
+        t = Template("""{% load admin_urls %}<a href="{% url opts|admin_urlname:'change' inst.pk %}">{{ inst }}</a>""")
+        return mark_safe(t.render(Context({ 'inst': inst, 'opts': inst._meta})))
+    return ""
