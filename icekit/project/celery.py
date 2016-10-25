5cd931046bc3c6dc2eb434325f4ff184b4855fa5
Mark Finger <markfinger@gmail.com>
2016-10-25 15:30:47 +1100

    Ensure that sentry tracks errors from celery tasks - re #38

--- icekit/project/celery.py
+++ icekit/project/celery.py
@@ -15,3 +15,13 @@ app = Celery('icekit')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# Sentry/raven integration for celery - placed after the celery initialisation
+# so that Raven is capable of wiring into the project's celery app
+from raven.contrib.django.raven_compat.models import client
+from raven.contrib.celery import register_signal, register_logger_signal
+
+# Register a custom filter to filter out duplicate logs
+register_logger_signal(client)
+# Hook into the Celery error handler
+register_signal(client)
