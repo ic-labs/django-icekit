451bcd32e97831899d15baa7374eac5331dc4d3d
James Murty <james@murty.co>
2016-11-02 15:27:01 +1100

    Fix search indexing of Author models, re #44
    - only index published authors
    - relocated text template to expected location
    - hack to activate site's default translation language if necessary, so
      following the `parent` link to a fluent page doesn't crash indexing.

--- icekit/page_types/author/search_indexes.py
+++ icekit/page_types/author/search_indexes.py
@@ -1,4 +1,8 @@
 from haystack import indexes
+
+from django.conf import settings
+from django.utils import translation
+
 from . import models
 
 
@@ -7,11 +11,20 @@ class AuthorIndex(indexes.SearchIndex, indexes.Indexable):
     Search index for `Author`.
     """
     text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='get_full_name', boost=2.0)
+    name = indexes.CharField(model_attr='title', boost=2.0)
     url = indexes.CharField(model_attr='get_absolute_url')
     has_url = indexes.BooleanField(model_attr='get_absolute_url')
     # We add this for autocomplete.
-    content_auto = indexes.EdgeNgramField(model_attr='get_full_name')
+    content_auto = indexes.EdgeNgramField(model_attr='title')
+
+    def index_queryset(self, using=None):
+        """ Index only published authors """
+        # TODO Hack to activate the site language if none is yet active, to
+        # avoid complaints about null language_code when traversing the
+        # `parent` relationship -- should probably do this elsewhere?
+        if not translation.get_language():
+            translation.activate(settings.LANGUAGE_CODE)
+        return self.get_model().objects.published()
 
     def get_model(self):
         """
