430ae423f4ac6074f8fd00e70f7a2d5cb431adf4
Aram Dulyan <aram00@gmail.com>
2016-11-07 16:21:16 +1100

    Avoid mixing tabs and spaces, even in .sh files

--- icekit/bin/pip-install.sh
+++ icekit/bin/pip-install.sh
@@ -16,7 +16,7 @@ cd "$DIR"
 if [[ -f requirements.txt ]]; then
 	if [[ ! -s requirements.txt.md5 ]] || ! md5sum --status -c requirements.txt.md5 > /dev/null 2>&1; then
 		echo "Python requirements in '$DIR' directory are out of date, 'requirements.txt' has been updated."
-        python -m pip install -r requirements.txt
+		python -m pip install -r requirements.txt
 		md5sum requirements.txt > requirements.txt.md5
 	fi
 fi
@@ -24,7 +24,7 @@ fi
 if [[ -f requirements-local.txt ]]; then
 	if [[ ! -s requirements-local.txt.md5 ]] || ! md5sum --status -c requirements-local.txt.md5 > /dev/null 2>&1; then
 		echo "Python requirements in '$DIR' directory are out of date, 'requirements-local.txt' has been updated."
-        python -m pip install -r requirements-local.txt
+		python -m pip install -r requirements-local.txt
 		md5sum requirements-local.txt > requirements-local.txt.md5
 	fi
 fi
