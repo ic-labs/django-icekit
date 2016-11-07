232bd6cd78f5355014f4ad13e2e5ab5860c2bf4f
Greg Turner <greg@interaction.net.au>
2016-11-07 19:45:24 +1100

    Make abstract and derive trivial concrete subclasses for Article and ArticleCategory.

---
+++ icekit/page_types/article/abstract_models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from icekit.content_collections.abstract_models import \
+    AbstractCollectedContent, AbstractListingPage, TitleSlugMixin
+from icekit.publishing.models import PublishableFluentContents
+
+
+class AbstractArticleCategoryPage(AbstractListingPage):
+    def get_items_to_list(self, request):
+        unpublished_pk = self.get_draft().pk
+        return Article.objects.published().filter(parent_id=unpublished_pk)
+
+    def get_items_to_mount(self, request):
+        unpublished_pk = self.get_draft().pk
+        return Article.objects.visible().filter(parent_id=unpublished_pk)
+
+    class Meta:
+        verbose_name = "Article category page"
+        abstract = True
+
+
+class AbstractArticle(
+    PublishableFluentContents,
+    AbstractCollectedContent,
+    TitleSlugMixin
+):
+    parent = models.ForeignKey(
+        'ArticleCategoryPage',
+        limit_choices_to={'publishing_is_draft': True},
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        unique_together = (('slug', 'parent', 'publishing_linked'), )
+        abstract = True
--- icekit/page_types/article/models.py
+++ icekit/page_types/article/models.py
@@ -1,29 +1,10 @@
-from django.db import models
-from icekit.content_collections.abstract_models import \
-    AbstractCollectedContent, AbstractListingPage, TitleSlugMixin
-from icekit.publishing.models import PublishableFluentContents
+from .abstract_models import \
+    AbstractArticleCategoryPage, AbstractArticle
 
 
-class ArticleCategoryPage(AbstractListingPage):
-    def get_items_to_list(self, request):
-        unpublished_pk = self.get_draft().pk
-        return Article.objects.published().filter(parent_id=unpublished_pk)
-
-    def get_items_to_mount(self, request):
-        unpublished_pk = self.get_draft().pk
-        return Article.objects.visible().filter(parent_id=unpublished_pk)
-
-    class Meta:
-        verbose_name = "Article category page"
+class ArticleCategoryPage(AbstractArticleCategoryPage):
+    pass
 
 
-class Article(
-        PublishableFluentContents, AbstractCollectedContent, TitleSlugMixin):
-    parent = models.ForeignKey(
-        'ArticleCategoryPage',
-        limit_choices_to={'publishing_is_draft': True},
-        on_delete=models.PROTECT,
-    )
-
-    class Meta:
-        unique_together = (('slug', 'parent', 'publishing_linked'), )
+class Article(AbstractArticle):
+    pass
