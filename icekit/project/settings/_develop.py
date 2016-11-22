307b792e1d472ec0df44bad13066ac020f91a186
Greg Turner <greg@interaction.net.au>
2016-11-22 11:48:53 +1100

    Re #149 disabling cache backend on develop as interim workaround

--- icekit/project/settings/_develop.py
+++ icekit/project/settings/_develop.py
@@ -6,10 +6,11 @@ SITE_PORT = 8000
 
 ALLOWED_HOSTS = ('*', )
 
-CACHES['default'].update({
-    'BACKEND': 'redis_lock.django_cache.RedisCache',
-    'LOCATION': 'redis://%s/1' % REDIS_ADDRESS,
-})
+# Disabling develop cache backend as short-term workaround until #149 is resolved
+# CACHES['default'].update({
+#     'BACKEND': 'redis_lock.django_cache.RedisCache',
+#     'LOCATION': 'redis://%s/1' % REDIS_ADDRESS,
+# })
 
 CSRF_COOKIE_SECURE = False  # Don't require HTTPS for CSRF cookie
 SESSION_COOKIE_SECURE = False  # Don't require HTTPS for session cookie
