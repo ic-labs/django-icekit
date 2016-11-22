50392c92551f6ac28664c51cb6f3720d13a5ea6a
Greg Turner <greg@interaction.net.au>
2016-11-22 23:54:22 +1100

    For some reason tests on child_pages decided to start failing due to DraftBoobyTrap access just now. This change repairs logic on the plugin to be aware of draft/public status.

--- icekit/plugins/child_pages/abstract_models.py
+++ icekit/plugins/child_pages/abstract_models.py
@@ -4,13 +4,20 @@ from fluent_contents.models import ContentItem
 
 
 @python_2_unicode_compatible
-class AbstractChildPageItem(ContentItem):
+class AbstractChildPagesItem(ContentItem):
     class Meta:
         abstract = True
-        verbose_name = _('Child Page')
+        verbose_name = _('Child Pages')
 
     def __str__(self):
         return 'Child Pages'
 
     def get_child_pages(self):
-        return self.parent.get_children()
+        # If my parent page is draft, show parent's (draft) children
+        # if my parent page is published, show published equivalents of parent's children
+        parent = self.parent
+
+        if parent.is_draft:
+            return [p for p in parent.get_children() if p.publishing_is_draft]
+        else:
+            return parent.get_draft().get_children().published()
