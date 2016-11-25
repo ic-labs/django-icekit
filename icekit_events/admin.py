5ec0eaf2a2d54b9aad4417c56bb41640d25897af
Greg Turner <greg@interaction.net.au>
2016-11-25 16:12:35 +1100

    Show new fields in admin listing.

--- icekit_events/admin.py
+++ icekit_events/admin.py
@@ -106,16 +106,19 @@ class EventTypeFilter(ChildModelFilter):
 
 class EventAdmin(ChildModelPluginPolymorphicParentModelAdmin,
                  publishing_admin.PublishingAdmin):
+    """
+    Polymorphic parent admin for Events.
+    """
     base_model = models.EventBase
     list_filter = (
-        EventTypeFilter, 'primary_type', 'secondary_types', 'modified', 'show_in_calendar',
+        EventTypeFilter, 'primary_type', 'secondary_types', 'modified', 'show_in_calendar', 'has_tickets_available',
         publishing_admin.PublishingStatusFilter,
         publishing_admin.PublishingPublishedFilter,
     )
     list_display = (
         '__str__', 'child_type_name', 'primary_type', 'modified',
         'publishing_column',
-        'part_of_display', 'show_in_calendar',
+        'part_of_display', 'show_in_calendar', 'has_tickets_available',
         'occurrence_count',
         'first_occurrence', 'last_occurrence',
     )
