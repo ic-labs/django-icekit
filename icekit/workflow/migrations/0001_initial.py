86c105821dc3948a007b6e442a21e00eac89cf70
Greg Turner <greg@interaction.net.au>
2016-11-28 11:08:27 +1100

    on_delete policy, updated choices.

--- icekit/workflow/migrations/0001_initial.py
+++ icekit/workflow/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
                 ('object_id', models.PositiveIntegerField()),
-                ('status', models.CharField(choices=[(b'', b''), (b'pending_review', b'Pending Review'), (b'approved', b'Approved')], max_length=254, default=b'')),
+                ('status', models.CharField(choices=[(b'new', b'New'), (b'ready_to_review', b'Ready to review'), (b'approved', b'Approved')], max_length=254, default=b'')),
                 ('assigned_to', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True, help_text=b'User responsible for item at this stage in the workflow')),
                 ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
             ],
