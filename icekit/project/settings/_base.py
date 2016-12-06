7b12f7d53e8941dbdc4cb50dbe2dae3818f392d1
Sam Minton <sam@interaction.net.au>
2016-12-06 15:51:32 +1100

    fix oembed installed app

--- icekit/project/settings/_base.py
+++ icekit/project/settings/_base.py
@@ -705,7 +705,7 @@ INSTALLED_APPS += (
     'icekit.plugins.links',
     'icekit.plugins.map',
     'icekit.plugins.map_with_text',
-    'icekit.plugins.oembed_with_caption.apps.OEmbedWithCaptionAppConfig',
+    'icekit.plugins.oembed_with_caption',
     # Replaces 'icekit.plugins.oembed_with_caption',
     # Includes fix for https://github.com/django-fluent/django-fluent-contents/issues/65
 
