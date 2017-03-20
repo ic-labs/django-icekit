48bf3c7e47bceffa5ef7501e3155640b66fb5213
Mark Finger <markfinger@gmail.com>
2017-03-21 08:53:59 +1100

    Added placeholder descriptors to layout pages. Prevented related content contains from rendering when no related content exists - re #197

--- icekit/page_types/layout_page/abstract_models.py
+++ icekit/page_types/layout_page/abstract_models.py
@@ -2,11 +2,14 @@ from fluent_pages.integration.fluent_contents import FluentContentsPage
 
 from icekit.models import ICEkitFluentContentsPageMixin
 from icekit.mixins import LayoutFieldMixin, HeroMixin, ListableMixin
+from icekit.plugins.descriptors import contribute_to_class as contribute_placeholder_descriptor_to_class
 
 
 class AbstractUnpublishableLayoutPage(FluentContentsPage, LayoutFieldMixin):
     class Meta:
         abstract = True
+
+contribute_placeholder_descriptor_to_class(AbstractUnpublishableLayoutPage, name='slots')
 
 
 class AbstractLayoutPage(ICEkitFluentContentsPageMixin, LayoutFieldMixin,
@@ -29,11 +32,13 @@ class AbstractLayoutPage(ICEkitFluentContentsPageMixin, LayoutFieldMixin,
     def get_related_items(self):
         items = []
 
-        related_placeholder = self.placeholder_set.filter(title='Related').first()
-        if related_placeholder:
-            items += list(related_placeholder.contentitems.all())
+        related_placeholder = self.slots.related
+        if related_placeholder.exists():
+            items += list(related_placeholder)
 
         if hasattr(self, 'get_auto_related_items'):
             items += list(self.get_auto_related_items())
 
         return items
+
+contribute_placeholder_descriptor_to_class(AbstractLayoutPage, name='slots')
