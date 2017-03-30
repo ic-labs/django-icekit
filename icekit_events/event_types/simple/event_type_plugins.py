b4ae1c56dd230bf1fc9b71fd55ec328065d914cd
Greg Turner <greg@interaction.net.au>
2017-03-30 18:50:01 +1100

    Give default events plugin a sort priority

--- icekit_events/event_types/simple/event_type_plugins.py
+++ icekit_events/event_types/simple/event_type_plugins.py
@@ -5,3 +5,4 @@ from icekit_events.admin import EventChildModelPlugin, EventWithLayoutsAdmin
 class BasicEventPlugin(EventChildModelPlugin):
     model = models.SimpleEvent
     model_admin = EventWithLayoutsAdmin
+    sort_priority = 11
