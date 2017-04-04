6aff402a132b967c8ffade88348765a0b71338d1
James Murty <james@murty.co>
2017-04-04 16:45:03 +1000

    #212 Make image API routes reversible

--- icekit/images_api/urls.py
+++ icekit/images_api/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 # Obtain the default router and register the ImageViewSet
 router = routers.DefaultRouter()
-router.register(r'images', views.ImageViewSet)
+router.register(r'images', views.ImageViewSet, 'images')
 
 # Define the URL patterns based upon the default router config.
 urlpatterns = [
-    url(r'^', include(router.urls, namespace='api-images')),
+    url(r'^', include(router.urls)),
 ]
