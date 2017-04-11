2959555a8c3d129b64a425a5053cfae0d1dfb3aa
James Murty <james@murty.co>
2017-04-11 15:10:05 +1000

    #213 Person API is now provided by GLAMkit Collections
    Person model name replaces the legacy Artist.

--- icekit/api/collection/views.py
+++ icekit/api/collection/views.py
@@ -1,47 +1,10 @@
-from django.db.models import Prefetch
-
-from glamkit_collection.models import ArtworkArtist as ArtworkArtistModel, \
-    Artist as ArtistModel, Artwork as ArtworkModel, \
-    Exhibition as ExhibitionModel
-
 from sfmoma.getty.models import GettyArtistData, GettyRelationships
 
 from ..base_views import RedirectViewset
 
-from . import artists
 from . import exhibitions
 from . import networks
 from . import relationships
-
-
-class ArtistViewSet(RedirectViewset):
-    """
-    Artist resource.
-    """
-    queryset = ArtistModel.objects.all().prefetch_related(Prefetch(
-        'artworkartist_set',
-        queryset=ArtworkArtistModel.objects.select_related('artwork')
-    ))
-
-    serializer_class = artists.serializers.Artist
-    redirect_view_name = 'artist-detail'
-    filter_class = artists.filters.ArtistFilter
-    ordering_fields = (
-            'name_display',
-            'name_sort',
-            'name_given',
-            'name_family',
-
-            'life_info_birth_date_sort_earliest',
-            'life_info_birth_date_sort_latest',
-            'life_info_death_date_sort_earliest',
-            'life_info_death_date_sort_latest',
-
-            'background_country',
-            'background_continent',
-    )
-
-
 
 
 class ExhibitionViewSet(RedirectViewset):
