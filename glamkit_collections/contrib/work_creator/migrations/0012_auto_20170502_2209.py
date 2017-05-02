f614502f0a67b754f5757935ffb0d86920998dcb
James Murty <james@murty.co>
2017-05-02 22:10:17 +1000

    Add DB migration for more explicit ordering of work & creator models

---
+++ glamkit_collections/contrib/work_creator/migrations/0012_auto_20170502_2209.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gk_collections_work_creator', '0011_role_title_plural'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='creatorbase',
+            options={'ordering': ('name_sort', 'slug', 'publishing_is_draft'), 'verbose_name': 'creator'},
+        ),
+        migrations.AlterModelOptions(
+            name='workbase',
+            options={'ordering': ('slug', 'publishing_is_draft'), 'verbose_name': 'work'},
+        ),
+    ]
