28ac10424b9f64c90a6b93bd99e203dba607460b
Tai Lee <real.human@mrmachine.net>
2017-05-05 23:10:18 +1000

    Update comments.

--- project_template/go.sh
+++ project_template/go.sh
@@ -8,7 +8,7 @@ EOF
 
 set -e
 
-# Get absolute project directory from the location of this script.
+# Get the absolute project directory from the location of this script.
 # See: http://stackoverflow.com/a/4774063
 export ICEKIT_PROJECT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd -P)
 
@@ -18,18 +18,18 @@ export ICEKIT_VENV="$ICEKIT_PROJECT_DIR/var/go.sh-venv"
 # Don't write `*.pyc` files.
 export PYTHONDONTWRITEBYTECODE=1
 
-# Create virtualenv.
+# Create a virtualenv.
 if [[ ! -d "$ICEKIT_VENV" ]]; then
     virtualenv "$ICEKIT_VENV"
 fi
 
-# Install ICEKit.
+# Install requirements.
 if [[ -z $("$ICEKIT_VENV/bin/python" -m pip freeze | grep django-icekit) ]]; then
 	bash -c "'$ICEKIT_VENV/bin/python' -m pip install --no-cache-dir --no-deps -r <(grep -v setuptools requirements.txt)"  # Unpin setuptools dependencies. See: https://github.com/pypa/pip/issues/4264
 	md5sum requirements.txt > requirements.txt.md5
 fi
 
-# Get absolute directory for the `icekit` package.
+# Get the absolute directory for the `icekit` package.
 export ICEKIT_DIR=$("$ICEKIT_VENV/bin/python" -c "import icekit, os, sys; sys.stdout.write('%s\n' % os.path.dirname(icekit.__file__));")
 
 # Execute entrypoint and command.
