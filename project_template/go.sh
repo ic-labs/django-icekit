2776232e50242fdb36d87a09b21a8b5ca1fa0594
Tai Lee <real.human@mrmachine.net>
2017-05-06 13:55:28 +1000

    Update to `ixc-django-docker` version of `go.sh`.

--- project_template/go.sh
+++ project_template/go.sh
@@ -8,25 +8,21 @@ EOF
 
 set -e
 
-# Get the absolute project directory from the location of this script.
+# Get absolute project directory from the location of this script.
 # See: http://stackoverflow.com/a/4774063
 export PROJECT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd -P)
 
 # Set location of virtualenv.
 export PROJECT_VENV_DIR="${VIRTUAL_ENV:-$PROJECT_DIR/var/go.sh-venv}"
 
-# Don't write `*.pyc` files.
-export PYTHONDONTWRITEBYTECODE=1
-
-# Create a virtualenv.
+# Create virtualenv.
 if [[ ! -d "$PROJECT_VENV_DIR" ]]; then
-    virtualenv "$PROJECT_VENV_DIR"
+	virtualenv "$PROJECT_VENV_DIR"
 fi
 
-# Install requirements.
-if [[ -z $("$PROJECT_VENV_DIR/bin/python" -m pip freeze | grep django-icekit) ]]; then
-	bash -c "'$PROJECT_VENV_DIR/bin/python' -m pip install --no-cache-dir --no-deps -r <(grep -v setuptools requirements.txt)"  # Unpin setuptools dependencies. See: https://github.com/pypa/pip/issues/4264
-	md5sum requirements.txt > requirements.txt.md5
+# Install `ixc-django-docker` package.
+if [[ -z $("$PROJECT_VENV_DIR/bin/python" -m pip freeze | grep ixc-django-docker) ]]; then
+	"$PROJECT_VENV_DIR/bin/python" -m pip install -e git+https://github.com/ixc/ixc-django-docker.git#egg=ixc-django-docker
 fi
 
 # Execute entrypoint and command.
