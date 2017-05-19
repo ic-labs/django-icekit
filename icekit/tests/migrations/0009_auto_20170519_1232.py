1f02d914b2e172f0421e27a326cd135d82e48a74
James Murty <james@murty.co>
2017-05-19 12:33:30 +1000

    #236 Add missing DB migration for unit test models

---
+++ icekit/tests/migrations/0009_auto_20170519_1232.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tests', '0008_auto_20161204_1456'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='articlelisting',
+            name='admin_notes',
+            field=models.TextField(help_text=b"Administrator's notes about this item", blank=True),
+        ),
+        migrations.AddField(
+            model_name='articlelisting',
+            name='brief',
+            field=models.TextField(help_text=b'A document brief describing the purpose of this item', blank=True),
+        ),
+        migrations.AddField(
+            model_name='layoutpagewithrelatedpages',
+            name='admin_notes',
+            field=models.TextField(help_text=b"Administrator's notes about this item", blank=True),
+        ),
+        migrations.AddField(
+            model_name='layoutpagewithrelatedpages',
+            name='brief',
+            field=models.TextField(help_text=b'A document brief describing the purpose of this item', blank=True),
+        ),
+    ]
