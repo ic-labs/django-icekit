7fc2e73e41d74375e03d57500ae552a4c1299781
James Murty <james@murty.co>
2017-06-15 16:37:30 +1000

    #247 Image API now supports writing media category relationships
    The Image API now takes advantage of the M2M support in
    `WritableSerializerHelperMixin` to permit media category
    instances to be related, created, and updated via the
    Image API endpoint.
    
    Unit tests exercise this feature, and give useful
    guidance for the arcane way nested lists of items
    must be specified in HTML multipart form data.

--- icekit/api/images/serializers.py
+++ icekit/api/images/serializers.py
@@ -2,20 +2,51 @@ from django.apps import apps
 
 from rest_framework import serializers
 
+from icekit.api.base_serializers import WritableSerializerHelperMixin, \
+    WritableRelatedFieldSettings
+
 
 Image = apps.get_model('icekit_plugins_image.Image')
+MediaCategory = apps.get_model('icekit.MediaCategory')
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class MediaCategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MediaCategory
+        fields = ['id', 'name']
+        extra_kwargs = {
+            'id': {
+                'read_only': False,
+                'required': False,
+            },
+            'name': {
+                'read_only': False,
+                'required': False,
+            },
+        }
+
+
+class ImageSerializer(
+        WritableSerializerHelperMixin, serializers.ModelSerializer
+):
     """
     A serializer for an ICEkit Image.
     """
+    categories = MediaCategorySerializer(
+        many=True,
+    )
+
     class Meta:
         model = Image
         fields = ['id', 'image', 'width', 'height', 'title', 'alt_text',
                   'caption', 'credit', 'source', 'external_ref', 'categories',
                   'license', 'notes', 'date_created', 'date_modified',
                   'is_ok_for_web', 'is_cropping_allowed']
+        writable_related_fields = {
+            'categories': WritableRelatedFieldSettings(
+                can_create=True, can_update=True),
+        }
 
 
 # TODO It is probably not a good idea to allow API user to set auto-gen ID
--- icekit/api/images/tests.py
+++ icekit/api/images/tests.py
@@ -1,9 +1,13 @@
+from django.apps import apps
+
 from django_dynamic_fixture import G
 
 from icekit.utils.testing import get_test_image, setup_with_context_manager
 
 from .. import base_tests
 from ..base_tests import Image
+
+MediaCategory = apps.get_model('icekit.MediaCategory')
 
 
 class ImageAPITests(base_tests._BaseAPITestCase):
@@ -137,3 +141,135 @@ class ImageAPITests(base_tests._BaseAPITestCase):
                 'image': open(self.image.image.file.name, 'rb'),
             }
         )
+
+    def test_add_image_with_post_and_relate_existing_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        category_1 = MediaCategory.objects.create(name='Category 1')
+        category_2 = MediaCategory.objects.create(name='Category 2')
+        response = self.client.post(
+            self.listing_url(),
+            {
+                'title': 'New image',
+                'image': open(self.image.image.file.name, 'rb'),
+                # See `rest_framework.utils.html.parse_html_list`
+                'categories[0]id': category_1.pk,
+                'categories[1]id': category_2.pk,
+            },
+            format='multipart',  # Cannot upload image with JSON
+        )
+        self.assertEqual(201, response.status_code)
+        new_image = Image.objects.get(pk=response.data['id'])
+        self.assertEqual('New image', new_image.title)
+        self.assertEqual(
+            set([category_1, category_2]),
+            set(new_image.categories.all()))
+
+    def test_add_image_with_post_and_create_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        response = self.client.patch(
+            self.detail_url(self.image.pk),
+            {
+                'title': 'New image',
+                # See `rest_framework.utils.html.parse_html_list`
+                'categories[0]name': 'New category 1',
+                'categories[1]name': 'New category 2',
+            },
+            format='multipart',  # Cannot upload image with JSON
+        )
+        self.assertEqual(200, response.status_code)
+        new_image = Image.objects.get(pk=response.data['id'])
+        self.assertEqual('New image', new_image.title)
+        self.assertEqual(
+            set(['New category 1', 'New category 2']),
+            set([c.name for c in new_image.categories.all()]))
+
+    def test_add_image_with_post_and_update_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        category_1 = MediaCategory.objects.create(name='Category 1')
+        category_2 = MediaCategory.objects.create(name='Category 2')
+        response = self.client.post(
+            self.listing_url(),
+            {
+                'title': 'New image',
+                'image': open(self.image.image.file.name, 'rb'),
+                # See `rest_framework.utils.html.parse_html_list`
+                'categories[0]id': category_1.pk,
+                'categories[0]name': 'Updated name 1',
+                'categories[1]id': category_2.pk,
+                'categories[1]name': 'Updated name 2',
+            },
+            format='multipart',  # Cannot upload image with JSON
+        )
+        self.assertEqual(201, response.status_code)
+        new_image = Image.objects.get(pk=response.data['id'])
+        self.assertEqual('New image', new_image.title)
+        self.assertEqual(
+            set([
+                (category_1.pk, 'Updated name 1'),
+                (category_2.pk, 'Updated name 2')
+            ]),
+            set([(c.pk, c.name) for c in new_image.categories.all()]))
+
+    def test_update_image_with_patch_and_relate_existing_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        category_1 = MediaCategory.objects.create(name='Category 1')
+        category_2 = MediaCategory.objects.create(name='Category 2')
+        response = self.client.patch(
+            self.detail_url(self.image.pk),
+            {
+                'title': 'Updated image',
+                'categories': [
+                    {'id': category_1.pk},
+                    {'id': category_2.pk},
+                ],
+            }
+        )
+        self.assertEqual(200, response.status_code)
+        updated_image = Image.objects.get(pk=self.image.pk)
+        self.assertEqual('Updated image', updated_image.title)
+        self.assertEqual(
+            set([category_1, category_2]),
+            set(updated_image.categories.all()))
+
+    def test_update_image_with_patch_and_create_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        response = self.client.patch(
+            self.detail_url(self.image.pk),
+            {
+                'title': 'Updated image',
+                'categories': [
+                    {'name': 'New category 1'},
+                    {'name': 'New category 2'},
+                ],
+            }
+        )
+        self.assertEqual(200, response.status_code)
+        updated_image = Image.objects.get(pk=self.image.pk)
+        self.assertEqual('Updated image', updated_image.title)
+        self.assertEqual(
+            set(['New category 1', 'New category 2']),
+            set([c.name for c in updated_image.categories.all()]))
+
+    def test_update_image_with_patch_and_update_mediacategories(self):
+        self.assertEqual(0, self.image.categories.count())
+        category_1 = MediaCategory.objects.create(name='Category 1')
+        category_2 = MediaCategory.objects.create(name='Category 2')
+        response = self.client.patch(
+            self.detail_url(self.image.pk),
+            {
+                'title': 'Updated image',
+                'categories': [
+                    {'id': category_1.pk, 'name': 'Updated name 1'},
+                    {'id': category_2.pk, 'name': 'Updated name 2'},
+                ],
+            }
+        )
+        self.assertEqual(200, response.status_code)
+        updated_image = Image.objects.get(pk=self.image.pk)
+        self.assertEqual('Updated image', updated_image.title)
+        self.assertEqual(
+            set([
+                (category_1.pk, 'Updated name 1'),
+                (category_2.pk, 'Updated name 2')
+            ]),
+            set([(c.pk, c.name) for c in updated_image.categories.all()]))
