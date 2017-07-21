2cddee5a147ee90c1e8c2e20c4893cb004cddc1d
James Murty <james@murty.co>
2017-07-21 15:42:30 +1000

    #261 Show upcoming event occurrences on Location detail page
    On the Location detail page show upcoming event
    occurrences visible to the current user.

--- icekit/plugins/location/views.py
+++ icekit/plugins/location/views.py
@@ -3,6 +3,8 @@ from django.template.response import TemplateResponse
 
 from icekit.publishing.utils import get_visible_object_or_404
 
+from icekit_events.models import Occurrence
+
 from .models import Location
 
 
@@ -17,8 +19,13 @@ def index(request):
 
 def location(request, slug):
     location = get_visible_object_or_404(Location, slug=slug)
+    upcoming_event_occurrences = Occurrence.objects \
+        .filter(event__location=location.get_draft()) \
+        .upcoming() \
+        .visible()  # Only show events visible to current user
     context = RequestContext(request, {
         'location': location,
+        'upcoming_event_occurrences': upcoming_event_occurrences,
     })
     template = 'icekit/plugins/location/location.html'
     return TemplateResponse(request, template, context)
