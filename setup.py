48961fe54819d1a0e5068714fb44f0652d213edf
requires.io <support@requires.io>
2020-12-09 00:08:25 +1100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setuptools.setup(
             'elasticstack',
         ],
         'test': [
-            'celery[redis]==5.0.3',
+            'celery[redis]==5.0.4',
             'coverage',
             'coveralls',
             'django-dynamic-fixture',
