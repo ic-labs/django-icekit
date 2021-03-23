c648bad918894a570f445780e054191abfaf6e08
requires.io <support@requires.io>
2021-03-24 05:15:11 +1100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setuptools.setup(
         'search': [
             'django-fluent-pages[flatpage,fluentpage]',
             'django-haystack',
-            'elasticsearch>=7.11,<7.12',
+            'elasticsearch>=7.12,<7.13',
             'elasticstack',
         ],
         'test': [
